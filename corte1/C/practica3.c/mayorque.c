705845d32e85e856938052e85deb1c5d9b1c7c80
valentina-arevalo <valeare1474@gmail.com>
2026-03-11 00:39:56 -0500

    Add mayorque.c to compare two integers

---
+++ corte1/C/practica3.c/mayorque.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int x, y ;
+   
+   printf("Ingrese el valor de x\n");
+  scanf("%d", &x);
+  printf("Ingrese el valor de y\n");
+  scanf("%d", &y);
+
+if (x > y) {
+    printf("x es mayor que y\n");
+  }
+  
+  return 0;
+}

7212b041a0e78d559364afcfbaf1b2dc959fe560
valentina-arevalo <valeare1474@gmail.com>
2026-03-24 19:39:16 -0500

    Remove imprimir function and its usage
    Removed the imprimir function and its call from main.

--- corte1/C/practica2/sumresmul.c
+++ corte1/C/practica2/sumresmul.c
@@ -15,10 +15,6 @@ int mult(int x1, int x2) {
         result=x1*x2;
         return result;
         }
-void imprimir (){
-        printf("Hola mundo\n");
-}
-
 
 int main() {
         int a, b, r;
@@ -28,7 +24,7 @@ int main() {
         printf("la suma de los resultados es %d \n", r);
         printf("la resta de los resultados es %d \n", resta(a, b));
         printf("la multiplicacion de los resultados es %d \n", mult(a, b)); 
-        imprimir();
+
         return 0;
 
 }

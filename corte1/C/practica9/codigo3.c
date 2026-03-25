51e2df3d18dac9feb7503a7b01aa42eb409af3d9
valentina-arevalo <valeare1474@gmail.com>
2026-03-24 20:29:15 -0500

    Remove duplicate variable declarations and clean code

---
+++ corte1/C/practica9/codigo3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main () {
+   int myage;
+  int votingage = 18;
+    printf("Ingrese la edad: \n");
+    scanf("%d", &myage);
+
+        if (myAge >= votingage) {
+                printf("Edad suficiente para votar.\n");
+        } 
+           else {
+                printf("No tienes edad para votar.\n");
+           }
+return 0;
+}

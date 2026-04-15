e8c520980c32286298697a40d66802f1f4fb35b7
valentina-arevalo <valeare1474@gmail.com>
2026-04-15 10:12:40 -0500

    Create for1.c

---
+++ corte2/C/for1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+ int main() {
+ int i; // Variable de control
+
+ for (i = 1; i <= 5; i++) { // Inicio, condicion y actualizacion
+printf(" %d\n", i); // Muestra el valor actual
+}
+ return 0;
+}

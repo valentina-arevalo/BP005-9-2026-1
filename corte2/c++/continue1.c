8853313d845528ffd332191082ebb5a24e574e7e
valentina-arevalo <valeare1474@gmail.com>
2026-04-16 23:18:44 -0500

    Add continue statement in loop example
    Implement a simple loop that skips the number 3.

---
+++ corte2/c++/continue1.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 5; i++) {
+        if (i == 3) {
+            continue;
+        }
+
+        cout << i << endl;
+    }
+
+    return 0;
+}

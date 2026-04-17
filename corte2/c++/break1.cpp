1ef4c75c2c3e37ab317385f8226c176610f3d42a
valentina-arevalo <valeare1474@gmail.com>
2026-04-16 23:20:53 -0500

    Add break statement in loop to exit at 6

---
+++ corte2/c++/break1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 10; i++) {
+        if (i == 6) {
+            break;
+        }
+
+        cout << i << endl;
+    }
+
+    return 0;
+}

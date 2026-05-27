c03df3c8e2e6a5302fcaaa1dd1b1bcbde2e23887
valentina-arevalo <valeare1474@gmail.com>
2026-05-27 10:35:36 -0500

    Update etapa5.ino

--- corte2/C/Practica Arduino/etapa5.ino
+++ corte2/C/Practica Arduino/etapa5.ino
@@ -1,80 +1,79 @@
-const int PIN_POTENCIOMETRO = A2;
-const int PIN_LED = 10;
-const int ADC_MAXIMAL = 1023;
-const int PWM_MAXIMAL = 255;
-const float VREF = 5.0;
+const int PIN_POTENCIOMETRO = A0;
 const int CANTIDAD_LECTURAS = 10;
-const int TIEMPO_ESPERA_MS = 20;
+const int PAUSA_ENTRE_LECTURAS_MS = 20;
 
-void tomarLecturas(int pin, int arreglo[], int cantidad, int tiempoMs) {
-    if (arreglo == nullptr || cantidad <= 0) return;
-    for (int i = 0; i < cantidad; i++) {
-        arreglo[i] = analogRead(pin);
-        delay(tiempoMs);
-    }
+void tomarLecturas(int pin, int datos[], int cantidad, int pausaMs) {
+  if (datos == nullptr || cantidad <= 0) return;
+
+  for (int i = 0; i < cantidad; i++) {
+    datos[i] = analogRead(pin);
+    delay(pausaMs);
+  }
 }
 
-void analizarLecturas(int arreglo[], int cantidad, int *promedio, int *minimo, int *maximo) {
-    if (arreglo == nullptr || promedio == nullptr || minimo == nullptr || maximo == nullptr || cantidad <= 0) return;
-    long suma = 0;
-    *minimo = arreglo[0];
-    *maximo = arreglo[0];
-    for (int i = 0; i < cantidad; i++) {
-        suma = suma + arreglo[i];
-        if (arreglo[i] < *minimo) *minimo = arreglo[i];
-        if (arreglo[i] > *maximo) *maximo = arreglo[i];
-    }
-    *promedio = (int)(suma / cantidad);
+void analizarLecturas(int datos[], int cantidad,
+                      int *promedio,
+                      int *minimo,
+                      int *maximo) {
+
+  if (datos == nullptr || promedio == nullptr || minimo == nullptr || maximo == nullptr || cantidad <= 0) {
+    return;
+  }
+
+  long suma = 0;
+
+  *minimo = datos[0];
+  *maximo = datos[0];
+
+  for (int i = 0; i < cantidad; i++) {
+    suma += datos[i];
+
+    if (datos[i] < *minimo) *minimo = datos[i];
+    if (datos[i] > *maximo) *maximo = datos[i];
+  }
+
+  *promedio = (int)(suma / cantidad);
 }
 
-void convertirPromedio(int promedio, float *voltaje, int *porcentaje, int *brillo) {
-    if (voltaje == nullptr || porcentaje == nullptr || brillo == nullptr) return;
-    *voltaje = (promedio * VREF) / ADC_MAXIMAL;
-    *porcentaje = (int)((long)promedio * 100L / ADC_MAXIMAL);
-    *brillo = (int)((long)promedio * PWM_MAXIMAL / ADC_MAXIMAL);
-}
+void mostrarArreglo(int datos[], int cantidad) {
+  if (datos == nullptr || cantidad <= 0) return;
 
-void aplicarSalidaLED(int pinLED, int brillo) {
-    analogWrite(pinLED, brillo); // Escritura nativa PWM
-}
+  Serial.print("Lecturas:");
 
-void imprimirArreglo(int arreglo[], int cantidad) {
-    if (arreglo == nullptr || cantidad <= 0) return;
-    Serial.print("Lecturas: ");
-    for (int i = 0; i < cantidad; i++) {
-        Serial.print(arreglo[i]);
-        Serial.print(" ");
-    }
-    Serial.println();
-}
+  for (int i = 0; i < cantidad; i++) {
+    Serial.print(datos[i]);
+    Serial.print(" ");
+  }
 
-void imprimirReporte(int promedio, int minimo, int maximo, float voltaje, int porcentaje, int brillo) {
-    Serial.print("Promedio ADC = "); Serial.println(promedio);
-    Serial.print("Minimo ADC = ");   Serial.println(minimo);
-    Serial.print("Maximo ADC = ");   Serial.println(maximo);
-    Serial.print("Voltaje = ");      Serial.print(voltaje, 2); Serial.println(" V");
-    Serial.print("Porcentaje = ");   Serial.print(porcentaje); Serial.println(" %");
-    Serial.print("Brillo PWM = ");   Serial.println(brillo);
-    Serial.println("----------------------------------------");
+  Serial.println();
 }
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(PIN_LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    int lecturas[CANTIDAD_LECTURAS];
-    int promedio = 0, minimo = 0, maximo = 0;
-    float voltaje = 0.0;
-    int porcentaje = 0, brillo = 0;
+  int lecturas[CANTIDAD_LECTURAS];
 
-    tomarLecturas(PIN_POTENCIOMETRO, lecturas, CANTIDAD_LECTURAS, TIEMPO_ESPERA_MS);
-    analizarLecturas(lecturas, CANTIDAD_LECTURAS, &promedio, &minimo, &maximo);
-    convertirPromedio(promedio, &voltaje, &porcentaje, &brillo);
-    aplicarSalidaLED(PIN_LED, brillo);
-    imprimirArreglo(lecturas, CANTIDAD_LECTURAS);
-    imprimirReporte(promedio, minimo, maximo, voltaje, porcentaje, brillo);
+  int promedio = 0;
+  int minimo = 0;
+  int maximo = 0;
 
-    delay(1000);
+  tomarLecturas(PIN_POTENCIOMETRO, lecturas, CANTIDAD_LECTURAS, PAUSA_ENTRE_LECTURAS_MS);
+  analizarLecturas(lecturas, CANTIDAD_LECTURAS, &promedio, &minimo, &maximo);
+
+  mostrarArreglo(lecturas, CANTIDAD_LECTURAS);
+
+  Serial.print("Promedio=");
+  Serial.println(promedio);
+
+  Serial.print("Minimo=");
+  Serial.println(minimo);
+
+  Serial.print("Maximo=");
+  Serial.println(maximo);
+
+  Serial.println("------------------------------");
+
+  delay(1000);
 }

6f60feead519e48e79071892c5f18004b2a174da
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 23:13:23 -0500

    Create cod11.c

---
+++ corte2/C/Arreglos, matrices, punteros/ejemplos/cod11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+int m [2] [3] = {
+{1, 2, 3},
+{4, 5, 6}
+};
+
+printf("m[0] [0] = %d\n", m[0] [0]);
+printf("m[0] [2] = %d\n", m[0] [2]);
+printf("m [1] [1] = %d\n", m[1] [1]);
+return 0;
+}

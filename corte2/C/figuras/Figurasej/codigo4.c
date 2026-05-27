062d2cba36185f9ded84f2b6d9ec548a4b4c4962
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 21:45:40 -0500

    Add pyramid pattern printing in codigo4.c
    This program prints a pyramid pattern using asterisks.

---
+++ corte2/C/figuras/Figurasej/codigo4.c
@@ -0,0 +1,19 @@
+ # include < stdio .h >
+
+ int main () {
+ int fila , espacios , simbolos ;
+
+for ( fila = 1; fila <= 5; fila ++) {
+ for ( espacios = 1; espacios <= 5 - fila ; espacios ++) {
+ printf (" ") ;
+ }
+
+ for ( simbolos = 1; simbolos <= 2 * fila - 1; simbolos ++) {
+ printf ("*") ;
+ }
+
+ printf ("\n") ;
+ }
+
+ return 0;
+ }

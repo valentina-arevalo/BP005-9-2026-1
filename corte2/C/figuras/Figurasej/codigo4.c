e2da6bdf4ecb7dbe5336faf2def6c7caa024be13
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 22:05:44 -0500

    Update codigo4.c

--- corte2/C/figuras/Figurasej/codigo4.c
+++ corte2/C/figuras/Figurasej/codigo4.c
@@ -1,14 +1,14 @@
- # include < stdio .h >
+ # include < stdio.h >
 
  int main () {
- int fila , espacios , simbolos ;
+ int fila, espacios, simbolos ;
 
-for ( fila = 1; fila <= 5; fila ++) {
- for ( espacios = 1; espacios <= 5 - fila ; espacios ++) {
+for (fila = 1; fila <= 5; fila ++) {
+ for (espacios = 1; espacios <= 5 - fila ; espacios ++) {
  printf (" ") ;
  }
 
- for ( simbolos = 1; simbolos <= 2 * fila - 1; simbolos ++) {
+ for (simbolos = 1; simbolos <= 2 * fila - 1; simbolos ++) {
  printf ("*") ;
  }
 

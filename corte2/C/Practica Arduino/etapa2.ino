b9b9962291d91eeb1663a7dcfeff1014d4e49d20
valentina-arevalo <valeare1474@gmail.com>
2026-05-27 10:32:47 -0500

    Update etapa2.ino

--- corte2/C/Practica Arduino/etapa2.ino
+++ corte2/C/Practica Arduino/etapa2.ino
@@ -1,43 +1,40 @@
-const int PIN_POT_ANALOGICO = A2;
-const int LIMIT_ADC = 1023;     
-const float VOLTS_REF = 5.0;   
+const int PIN_POTENCIOMETRO = A0;
+const int ADC_MAX = 1023;
+const float VREF = 5.0;
 
-int capturarPin(int pinHardware) {
-    int muestraRaw = analogRead(pinHardware);
-    return muestraRaw;
+int leerADC(int pin) {
+  return analogRead(pin);
 }
 
-float obtenerVoltios(int muestraRaw) {
-    float tension = (muestraRaw * VOLTS_REF) / LIMIT_ADC;
-    return tension;
+float calcularVoltaje(int valorADC) {
+  return (valorADC * VREF) / ADC_MAX;
 }
 
-int obtenerEscalaPrc(int muestraRaw) {
-    int porcentajeVal = (int)((long)muestraRaw * 100L / LIMIT_ADC);
-    return porcentajeVal;
+int calcularPorcentaje(int valorADC) {
+  return (int)((long)valorADC * 100L / ADC_MAX);
 }
 
-void mostrarReporteConsola(int muestraRaw, float tension, int porcentajeVal) {
-    Serial.print("Muestra = ");
-    Serial.print(muestraRaw);
-    Serial.print(" | Tension = ");
-    Serial.print(tension, 2);
-    Serial.print("V");
-    Serial.print(" | Nivel = ");
-    Serial.print(porcentajeVal);
-    Serial.println("%");
+void mostrarDatos(int valorADC, float voltaje, int porcentaje) {
+  Serial.print("ADC=");
+  Serial.print(valorADC);
+  Serial.print("|Voltaje=");
+  Serial.print(voltaje, 2);
+  Serial.print("V");
+  Serial.print("|Porcentaje=");
+  Serial.print(porcentaje);
+  Serial.println(" %");
 }
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-    int muestraRaw = capturarPin(PIN_POT_ANALOGICO);
-    float tension = obtenerVoltios(muestraRaw);
-    int porcentajeVal = obtenerEscalaPrc(muestraRaw);
-    
-    mostrarReporteConsola(muestraRaw, tension, porcentajeVal);
-    
-    delay(500);
+  int valorADC = leerADC(PIN_POTENCIOMETRO);
+  float voltaje = calcularVoltaje(valorADC);
+  int porcentaje = calcularPorcentaje(valorADC);
+
+  mostrarDatos(valorADC, voltaje, porcentaje);
+
+  delay(500);
 }

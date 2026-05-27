14856bcc07e246221badd58191149a22a44d6a3d
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 23:02:46 -0500

    Create cod1.c

---
+++ corte2/C/Arreglos, matrices, punteros/ejemplos/cod1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+int datos [5] = {8, 3, 15, 6, 10};
+
+printf("datos [0] = %d\n", datos [0]);
+printf("datos [1] = %d\n", datos [1]);
+printf("datos [2] = %d\n", datos [2]);
+printf("datos [3] = %d\n", datos [3]);
+printf("datos [4] = %d\n", datos [4]);
+return 0;
+}

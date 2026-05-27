bada6cf80dc70c0dd4d55b58df7a76acb7671ac2
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 23:07:02 -0500

    Add cod6.c to display array elements

---
+++ corte2/C/Arreglos, matrices, punteros/ejemplos/cod6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void mostrarArreglo (int datos [], int n) {
+int i;
+
+for (i=0; i<n; i++) {
+
+printf("%d ", datos [i]);
+}
+printf("\n");
+}
+int main() {
+
+int datos [5] = {8, 3, 15, 6, 10};
+mostrarArreglo (datos, 5);
+return 0;
+}

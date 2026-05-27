8a8d74ff0810e809aaccf66586dec032c9f81dd5
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 21:44:48 -0500

    Add code to print a right-angled triangle pattern

---
+++ corte2/C/figuras/Figurasej/codigo3.c
@@ -0,0 +1,14 @@
+ # include <stdio .h>
+
+ int main () {
+ int fila , columna ;
+
+ for ( fila = 1; fila <= 5; fila ++) {
+ for ( columna = 1; columna <= fila ; columna ++) {
+ printf ("@ ");
+ }
+ printf ("\n") ;
+ }
+
+ return 0;
+ }

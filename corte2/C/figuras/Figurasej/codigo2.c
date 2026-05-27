f55982f1741c110b14e4a5177134e150c2320511
valentina-arevalo <valeare1474@gmail.com>
2026-05-26 21:44:02 -0500

    Add code to print a rectangle pattern

---
+++ corte2/C/figuras/Figurasej/codigo2.c
@@ -0,0 +1,14 @@
+ # include <stdio .h>
+
+ int main () {
+ int fila , columna ;
+
+ for ( fila = 1; fila <= 4; fila ++) {
+ for ( columna = 1; columna <= 8; columna ++) {
+ printf ("# ");
+ }
+ printf ("\n") ;
+ }
+
+ return 0;
+ }

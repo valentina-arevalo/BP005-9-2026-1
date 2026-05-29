748c2a8f0f80e1732c10b199608bf91b548f4166
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 20:34:35 -0500

    Update cod2.cpp

--- corte2/c++/arreglos, matrices, punteros/cod2.cpp
+++ corte2/c++/arreglos, matrices, punteros/cod2.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
 using namespace std;
-#include <stdlib.h>
+
 int main() {
-int n;
-int i;
-int *datos = NULL;
 
-cout << "Cuantos enteros desea guardar: ";
-cin >> "%d", &n;
-datos = malloc(n * sizeof(int);
+    int datos[5] = {8, 3, 15, 6, 10};
 
-if (datos == NULL) {
-cout << "No se pudo reservar memoria.\n";
-return 1;
-}
+    for (int i = 0; i < 5; i++) {
 
-for (i=0; i<n; i++) {
-cout << "Ingrese datos [%d]: ", i;
-cin >> "%d", &datos[i];
-}
-cout << "Datos ingresados: ";
-for (i=0; i < n; i++) {
-cout << "%d ", datos [i];
-}
-cout << "\n";
-free(datos;
+        cout << "datos[" << i << "] = "
+             << datos[i] << endl;
+    }
 
-datos = NULL;
-return 0;
+    return 0;
 }

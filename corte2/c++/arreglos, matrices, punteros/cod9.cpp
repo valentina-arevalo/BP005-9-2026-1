d90618f46bde567b8cd246e74775917f82361831
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 20:38:52 -0500

    Update cod9.cpp

--- corte2/c++/arreglos, matrices, punteros/cod9.cpp
+++ corte2/c++/arreglos, matrices, punteros/cod9.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 using namespace std;
-void mostrarArreglo (int *datos, int n) {
 
-int i;
+void analizarArreglo(int *datos,
+                     int n,
+                     int *suma,
+                     int *mayor,
+                     int *menor) {
 
-if (datos == NULL) {
-cout << "El arreglo no es valido.\n";
-return;
+    if (datos == nullptr ||
+        suma == nullptr ||
+        mayor == nullptr ||
+        menor == nullptr) {
+
+        return;
+    }
+
+    *suma = 0;
+
+    *mayor = datos[0];
+    *menor = datos[0];
+
+    for (int i = 0; i < n; i++) {
+
+        *suma = *suma + datos[i];
+
+        if (datos[i] > *mayor) {
+
+            *mayor = datos[i];
+        }
+
+        if (datos[i] < *menor) {
+
+            *menor = datos[i];
+        }
+    }
 }
 
-for (i=0; i < n; i++) {
-cout << "%d ", datos [i];
-}
-cout << "\n";
-}
 int main() {
 
-int datos [5] = {8, 3, 15, 6, 10};
+    int datos[5] = {8, 3, 15, 6, 10};
 
-mostrarArreglo (datos, 5;
-return 0;
+    int suma;
+    int mayor;
+    int menor;
+
+    analizarArreglo(datos,
+                     5,
+                     &suma,
+                     &mayor,
+                     &menor);
+
+    cout << "Suma = "
+         << suma << endl;
+
+    cout << "Mayor = "
+         << mayor << endl;
+
+    cout << "Menor = "
+         << menor << endl;
+
+    return 0;
 }

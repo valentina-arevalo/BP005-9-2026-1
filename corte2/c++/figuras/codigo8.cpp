78557b1d2363ca8c949eeee91365b8c1ae9a8afd
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 22:34:59 -0500

    Update codigo8.cpp

--- corte2/c++/figuras/codigo8.cpp
+++ corte2/c++/figuras/codigo8.cpp
@@ -1,32 +1,23 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 
- int main () {
- int fila , columna ;
+int main() {
+    int fila = 1;
+    int columna;
 
- for ( fila = 1; fila <= 11; fila ++) {
- for ( columna = 1; columna <= 11; columna ++) {
- if (( fila == 1 && ( columna == 3 || columna == 9) ) ||
- ( fila == 2 && ( columna == 4 || columna == 8) ) ||
- ( fila == 3 && ( columna >= 4 && columna <= 8) ) ||
- ( fila == 4 && ( columna >= 2 && columna <= 10) ) ||
- ( fila == 5 && ( columna >= 3 && columna <= 9) ) ||
- ( fila == 6 && ( columna >= 4 && columna <= 8) ) ||
- ( fila == 7 && ( columna == 2 || columna == 10 ||
- columna == 6) ) ||
- ( fila == 8 && ( columna == 3 || columna == 9 ||
-columna == 6) ) ||
- ( fila == 9 && ( columna == 4 || columna == 8 ||
-columna == 6) ) ||
- ( fila == 10 && ( columna == 5 || columna == 7) ) ||
- ( fila == 11 && columna == 6) ) {
- printf ("o";
- } else {
- printf (" ";
- }
- }
- printf ("\n") ;
- }
+    do {
+        columna = 1;
 
- return 0;
- }
+        do {
+            cout << "$ ";
+            columna++;
+
+        } while (columna <= fila);
+
+        cout << endl;
+        fila++;
+
+    } while (fila <= 5);
+
+    return 0;
+}

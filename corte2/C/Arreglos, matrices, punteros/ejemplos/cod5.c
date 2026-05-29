d61bf3730cabe74964cbbacac758608154b0dcf4
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 20:56:01 -0500

    Update cod5.c

--- corte2/C/Arreglos, matrices, punteros/ejemplos/cod5.c
+++ corte2/C/Arreglos, matrices, punteros/ejemplos/cod5.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+
 int main() {
 
-int datos [3] = {10, 20, 30};
-int *p = datos;
+    int datos[3] = {10, 20, 30};
 
-printf("datos [1] = %d\n", datos [1]);
-printf("*(datos + 1) = %d\n", *(datos + 1));
-printf("*(p+1)=%d\backslash n^{\prime\prime},*(p+1))$;
-printf("p [1] = %d\n", p[1]);
-return 0;
+    int *p = datos;
+
+    printf("datos[1] = %d\n", datos[1]);
+
+    printf("*(datos + 1) = %d\n", *(datos + 1));
+
+    printf("*(p + 1) = %d\n", *(p + 1));
+
+    printf("p[1] = %d\n", p[1]);
+
+    return 0;
 }

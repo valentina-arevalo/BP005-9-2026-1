42e9a85aee3a74b45a4c3b22d9c3b6df127bb838
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 20:44:49 -0500

    Update cod17.cpp

--- corte2/c++/arreglos, matrices, punteros/cod17.cpp
+++ corte2/c++/arreglos, matrices, punteros/cod17.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-/* Declaramos una matriz de 2 filas y 3 columnas. */
-int m [2] [3] = {
-{1, 2, 3},
-{4, 5, 6}
-};
-/* p apunta al primer entero de la matriz. */
-int *p=&m[0][0];
-/* total indica cuantos elementos tiene la matriz. */
-int total = 2 * 3;
-/* k recorre la matriz como una secuencia lineal. */
-int k;
-/* Mostramos todos los elementos usando *(p+k). */
-for (k=0; k < total; k++) {
-cout << "%d ", *(p + k);
-}
-cout << "\n";
-return 0;
+
+    int filas = 2;
+    int columnas = 3;
+
+    int *matriz = new int[filas * columnas];
+
+    for (int i = 0; i < filas; i++) {
+
+        for (int j = 0; j < columnas; j++) {
+
+            matriz[i * columnas + j] =
+            (i + 1) * 10 + j;
+        }
+    }
+
+    for (int i = 0; i < filas; i++) {
+
+        for (int j = 0; j < columnas; j++) {
+
+            cout << matriz[i * columnas + j]
+                 << " ";
+        }
+
+        cout << endl;
+    }
+
+    delete[] matriz;
+
+    matriz = nullptr;
+
+    return 0;
 }

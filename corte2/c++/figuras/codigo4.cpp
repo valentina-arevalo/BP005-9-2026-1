654fe9af7584424e5ea3d5f7321dd6994d465da3
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 22:28:21 -0500

    Update codigo4.cpp

--- corte2/c++/figuras/codigo4.cpp
+++ corte2/c++/figuras/codigo4.cpp
@@ -1,31 +1,21 @@
-# include <stdio.h>
+#include <iostream>
+using namespace std;
 
- int main () {
- int fila , espacios , simbolos ;
- int n = 5;
+int main() {
+    int fila, espacios, simbolos;
 
- for ( fila = 1; fila <= n; fila ++) {
- for ( espacios = 1; espacios <= n - fila ; espacios ++) {
- printf (" ") ;
- }
- for ( simbolos = 1; simbolos <= 2 * fila - 1; simbolos ++) {
- printf ("*") ;
- }
- printf ("\n") ;
- }
+    for (fila = 1; fila <= 5; fila++) {
 
- for ( fila = n - 1; fila >= 1; fila --) {
- for ( espacios = 1; espacios <= n - fila ; espacios ++) {
- printf (" ") ;
- }
- for ( simbolos = 1; simbolos <= 2 * fila - 1; simbolos ++) {
- printf ("*") ;
- }
- printf ("\n") ;
- }
+        for (espacios = 1; espacios <= 5 - fila; espacios++) {
+            cout << " ";
+        }
 
- return 0;
- }
+        for (simbolos = 1; simbolos <= 2 * fila - 1; simbolos++) {
+            cout << "*";
+        }
 
+        cout << endl;
+    }
 
-
+    return 0;
+}

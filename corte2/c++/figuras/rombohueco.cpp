7b59dd9fc6d52f8e3f15c2f49727d6f19c4cd94d
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 22:59:39 -0500

    Update rombohueco.cpp

--- corte2/c++/figuras/rombohueco.cpp
+++ corte2/c++/figuras/rombohueco.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 int main() {
-    int i, j;
+    int fila, col;
+    int n = 5;
+    int ancho = 2 * n - 1;
 
-    for(i = 1; i <= 7; i++) {
-        for(j = 1; j <= i; j++) {
-            cout << "+ ";
+    for (fila = 1; fila <= ancho; fila++) {
+
+        int dist = (fila <= n) ? n - fila : fila - n;
+
+        for (col = 1; col <= ancho; col++) {
+
+            if (col == dist + 1 || col == ancho - dist)
+                cout << "$";
+            else
+                cout << " ";
+
         }
-        cout << "\n";
+
+        cout << endl;
     }
 
     return 0;

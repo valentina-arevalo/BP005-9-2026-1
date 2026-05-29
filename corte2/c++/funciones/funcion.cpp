7fe60dfceb4a1b0b33e9bf1370ccdb6414613b66
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 21:32:27 -0500

    Update and rename celfar.cpp to funcion.cpp

---
+++ corte2/c++/funciones/funcion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void saludar();
+
+int main()
+{
+    saludar();
+    return 0;
+}
+
+void saludar()
+{
+    cout << "Hola. Estamos estudiando funciones." << endl;
+}

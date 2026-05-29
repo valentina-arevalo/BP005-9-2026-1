b8502f865677324d776afc37d6d33bda3e79dc12
valentina-arevalo <valeare1474@gmail.com>
2026-05-28 20:35:10 -0500

    Update cod3.cpp

--- corte2/c++/arreglos, matrices, punteros/cod3.cpp
+++ corte2/c++/arreglos, matrices, punteros/cod3.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
 using namespace std;
-void analizarArreglo (int *datos, int n, int *suma, int *mayor, int *menor) {
-int i;
 
-if (datos == NULL || suma == NULL || mayor == NULL || menor == NULL) {
-return;
-}
-*suma = 0;
-
-*mayor = datos [0];
-*menor = datos [0];
-
-for (i=0; i<n; i++) {
-*suma = *suma + datos [i];
-
-if (datos [i] > *mayor) {
-*mayor = datos [i];
-}
-if (datos [i] < *menor) {
-*menor = datos [i];
-}
-}
-}
 int main() {
 
-int datos [5] = {8, 3, 15, 6, 10};
-int suma;
-int mayor;
-int menor;
-analizarArreglo (datos, 5, &suma, &mayor, &menor;
+    int datos[5] = {8, 3, 15, 6, 10};
 
-cout << "Suma = %d\n", suma;
-cout << "Mayor = %d\n", mayor;
-cout << "Menor = %d\n", menor;
-return 0;
+    for (int i = 0; i < 5; i++) {
+
+        cout << "datos[" << i << "] = "
+             << datos[i]
+             << ", direccion = "
+             << &datos[i]
+             << endl;
+    }
+
+    return 0;
 }

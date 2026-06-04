89cf1a05f3e97e73d8737f7a60277c8003f9476e
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:50:02 -0500

    Create areatriangulo3.cpp

---
+++ corte2/c++/Binarios/ejercicios/areatriangulo3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double base, altura;
+
+    cin >> base;
+    cin >> altura;
+
+    cout << "Area: " << (base * altura) / 2 << endl;
+
+    return 0;
+}

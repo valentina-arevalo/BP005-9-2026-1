5af8ad03ccaa354810ecbb68319637093e481410
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:00:50 -0500

    Refactor suma1.cpp to remove duplicate code
    Removed duplicate main function and unnecessary code.

--- corte2/c++/Binarios/ejercicios/suma1.cpp
+++ corte2/c++/Binarios/ejercicios/suma1.cpp
@@ -3,19 +3,6 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    int b = 20;
-    int c = 30;
-
-    cout << a + b + c << endl;
-
-    return 0;
-}
-#include <iostream>
-
-using namespace std;
-
-int main() {
    
     int a = 10;
     int b = 20;

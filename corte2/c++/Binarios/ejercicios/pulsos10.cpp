cf3f83e35acece3dc42f3940a7b51d2fbe76d4a1
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:15:13 -0500

    Update pulsos10.cpp
    Removed comments for pulse variable and output.

--- corte2/c++/Binarios/ejercicios/pulsos10.cpp
+++ corte2/c++/Binarios/ejercicios/pulsos10.cpp
@@ -4,10 +4,8 @@
 using namespace std;
 
 int main() {
-    // Variable para contar pulsos
     uint16_t pulsos = 3500;
 
-    // Mostrar valor
     cout << "Pulsos: " << pulsos << endl;
 
     return 0;

023d4ad06862bd73e5c85bf607bd049170d7cf1b
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:02:23 -0500

    Update operaciones2.cpp
    Added checks to prevent division by zero and updated output messages accordingly.

--- corte2/c++/Binarios/ejercicios/operaciones2.cpp
+++ corte2/c++/Binarios/ejercicios/operaciones2.cpp
@@ -3,16 +3,25 @@
 using namespace std;
 
 int main() {
-    int a, b;
+    int a;
+    int b;
 
+    cout << "Ingrese el primer numero: ";
     cin >> a;
+
+    cout << "Ingrese el segundo numero: ";
     cin >> b;
 
     cout << "Suma: " << a + b << endl;
     cout << "Resta: " << a - b << endl;
     cout << "Multiplicacion: " << a * b << endl;
-    cout << "Division: " << a / b << endl;
-    cout << "Residuo: " << a % b << endl;
+
+    if (b != 0) {
+        cout << "Division: " << a / b << endl;
+        cout << "Residuo: " << a % b << endl;
+    } else {
+        cout << "No se puede dividir entre cero." << endl;
+    }
 
     return 0;
 }

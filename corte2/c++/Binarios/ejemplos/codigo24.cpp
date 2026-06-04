0b1872f9fc655aadb923221fda1c9de9355094ae
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:45:52 -0500

    Add codigo24.cpp to demonstrate int8_t overflow

---
+++ corte2/c++/Binarios/ejemplos/codigo24.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main() {
+    int8_t valor = 127;
+
+    valor = valor + 1;
+
+    cout << static_cast<int>(valor) << endl;
+
+    return 0;
+}

f1e10fda9a2c4ea84788455da99b8b608f307ab1
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:33:21 -0500

    Create codigo5.cpp

---
+++ corte2/c++/Binarios/ejemplos/codigo5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double temperatura = 24.5;
+    double distancia = 1.75;
+    double voltaje = 3.3;
+
+    cout << temperatura << endl;
+    cout << distancia << endl;
+    cout << voltaje << endl;
+
+    return 0;
+}

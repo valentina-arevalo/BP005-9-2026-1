c944d32968ac888739aff8f3ba816c5dd5739e99
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:36:02 -0500

    Create codigo10.cpp

---
+++ corte2/c++/Binarios/ejemplos/codigo10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int resultado1 = 2 + 3 * 4;
+    int resultado2 = (2 + 3) * 4;
+
+    cout << resultado1 << endl;
+    cout << resultado2 << endl;
+
+    return 0;
+}

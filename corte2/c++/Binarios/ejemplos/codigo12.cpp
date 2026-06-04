8ff7fa96b458a3a2338a040981bda040feee756c
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:37:01 -0500

    Create codigo12.cpp

---
+++ corte2/c++/Binarios/ejemplos/codigo12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double temperatura;
+
+    cout << "Ingrese la temperatura: ";
+    cin >> temperatura;
+
+    cout << temperatura << endl;
+
+    return 0;
+}

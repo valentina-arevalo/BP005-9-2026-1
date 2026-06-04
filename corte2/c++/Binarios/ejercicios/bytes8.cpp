8cdcf0f5d7d9b05884e5e368893fe72e59850a66
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:11:46 -0500

    Create bytes8.cpp

---
+++ corte2/c++/Binarios/ejercicios/bytes8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main() {  
+    cout << "int: " << sizeof(int) << endl;
+    cout << "float: " << sizeof(float) << endl;
+    cout << "double: " << sizeof(double) << endl;
+    cout << "char: " << sizeof(char) << endl;
+    cout << "bool: " << sizeof(bool) << endl;
+    cout << "uint8_t: " << sizeof(uint8_t) << endl;
+    cout << "uint16_t: " << sizeof(uint16_t) << endl;
+    cout << "uint32_t: " << sizeof(uint32_t) << endl;
+
+    return 0;
+}

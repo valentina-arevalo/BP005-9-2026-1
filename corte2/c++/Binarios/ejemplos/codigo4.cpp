61e4df63a7c31b5cb2a2e1bcf37c50aeacad2500
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:32:26 -0500

    Create codigo4.cpp

---
+++ corte2/c++/Binarios/ejemplos/codigo4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int edad = 18;
+    int estudiantes = 35;
+    int contador = 0;
+
+    cout << edad << endl;
+    cout << estudiantes << endl;
+    cout << contador << endl;
+
+    return 0;
+}

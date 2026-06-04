8f9a27f051f0434d35c4fc9b4188361abc094a55
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:16:49 -0500

    Create milisegundos11.cpp

---
+++ corte2/c++/Binarios/ejercicios/milisegundos11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main() {
+    uint32_t milisegundos = 500000;
+
+    cout << "Milisegundos: "
+         << milisegundos
+         << endl;
+
+    return 0;
+}

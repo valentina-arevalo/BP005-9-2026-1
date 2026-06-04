4ca523cfa2460342014727828c8f4e2b1e8d0d62
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:00:25 -0500

    Update suma1.cpp

--- corte2/c++/Binarios/ejercicios/suma1.cpp
+++ corte2/c++/Binarios/ejercicios/suma1.cpp
@@ -11,3 +11,17 @@ int main() {
 
     return 0;
 }
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    cout << "La suma es: " << a + b + c << endl;
+
+    return 0;
+}

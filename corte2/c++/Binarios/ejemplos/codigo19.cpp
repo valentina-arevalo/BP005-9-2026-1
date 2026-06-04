4d519c7bd8c7fa14a104a878100baecbafff262d
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:43:01 -0500

    Create codigo19.cpp

---
+++ corte2/c++/Binarios/ejemplos/codigo19.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double x = 16.0;
+    double base = 2.0;
+    double exponente = 3.0;
+
+    cout << sqrt(x) << endl;
+    cout << pow(base, exponente) << endl;
+    cout << fabs(-3.5) << endl;
+    cout << round(3.6) << endl;
+    cout << floor(3.9) << endl;
+    cout << ceil(3.1) << endl;
+
+    return 0;
+}

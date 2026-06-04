6c08ead3c25dcf00f26a5ad3494961db20faac60
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 22:49:20 -0500

    Add operaciones2.cpp for basic arithmetic operations

---
+++ corte2/c++/Binarios/ejercicios/operaciones2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << "Suma: " << a + b << endl;
+    cout << "Resta: " << a - b << endl;
+    cout << "Multiplicacion: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Residuo: " << a % b << endl;
+
+    return 0;
+}

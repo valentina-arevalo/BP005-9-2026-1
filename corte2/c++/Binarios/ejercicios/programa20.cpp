6365115c5592ea53a146f87859aa672f62646789
valentina-arevalo <valeare1474@gmail.com>
2026-06-03 23:40:35 -0500

    Add programa20.cpp with math function examples

---
+++ corte2/c++/Binarios/ejercicios/programa20.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "sqrt(16) = "
+         << sqrt(16) << endl;
+
+    cout << "pow(2,3) = "
+         << pow(2,3) << endl;
+
+    cout << "fabs(-3.5) = "
+         << fabs(-3.5) << endl;
+
+    cout << "round(3.6) = "
+         << round(3.6) << endl;
+
+    cout << "floor(3.9) = "
+         << floor(3.9) << endl;
+
+    cout << "ceil(3.1) = "
+         << ceil(3.1) << endl;
+
+    return 0;
+}
